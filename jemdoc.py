7be6b6606ec566542bcc647859ff736f18434fef
Junjie Jiao <34710033+junjiejiao@users.noreply.github.com>
2023-09-13 22:49:58 +0200

    Add files via upload

--- jemdoc.py
+++ jemdoc.py
@@ -1254,7 +1254,7 @@ def procfile(f):
   showfooter = True
   showsourcelink = False
   showlastupdated = True
-  showlastupdatedtime = True
+  showlastupdatedtime = False
   nodefaultcss = False
   fwtitle = False
   css = []
